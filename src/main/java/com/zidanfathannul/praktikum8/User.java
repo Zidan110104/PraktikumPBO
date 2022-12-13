6c667086b3abfb578912bdc54e3a8b0dcc221d49
Sony <Sony@DESKTOP-SFGMJOP>
2022-12-13 11:42:32 +0700

    Pertemuan8

---
+++ src/main/java/com/zidanfathannul/praktikum8/User.java
@@ -0,0 +1,21 @@
+package com.zidanfathannul.praktikum8;
+
+
+public class User {
+    
+    String email;
+    String password;
+    
+    public User(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+    
+    public boolean verify(String email, String password){
+        if(this.email.equals(email)&& this.password.equals(password))
+            return true;
+        
+        return false;
+    }
+    
+}
